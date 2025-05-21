d9fe4aad0e744e9075c7e96fb492336488f53912
Christian Graumann <christian@Mac.fritz.box>
2025-05-21 22:32:02 +0200

    8.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.188.17-8.18

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import Login from "./components/Login";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("library-user-token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+  };
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {token ? (
+          <>
+            <button onClick={() => setPage("add")}>add book</button>
+            <button onClick={logout}>logout</button>
+          </>
+        ) : (
+          <button onClick={() => setPage("login")}>login</button>
+        )}
       </div>
 
       <Authors show={page === "authors"} />
@@ -19,6 +40,8 @@ const App = () => {
       <Books show={page === "books"} />
 
       <NewBook show={page === "add"} />
+
+      <Login show={page === "login"} setToken={setToken} />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import App from "./App.jsx";
 
-const client = new ApolloClient({
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("library-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({
   uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
